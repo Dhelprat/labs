64e39abc9ea722709013a1555015575eb206b975
Dhelprat <indoorbox@yandex.ru>
2023-11-30 22:33:21 +0300

    переделано чуть-чуть

--- 6/main.c
+++ 6/main.c
@@ -11,6 +11,7 @@ int main() {
         switch (state) {
             case 0:
                 if ( isdigit(s) ) { num[last_ind] = s; last_ind ++; state = 1; }
+                else if ( isalpha(s) ) { last_ind = 0; memset(num, '\0', sizeof num); state = 4; }
                 else { state = 0; }
 
                 break;
@@ -18,29 +19,31 @@ int main() {
             case 1:
                 if ( isdigit(s) ) { state = 1; num[last_ind] = s; last_ind ++; }
                 else if ( s == 'i' ) { state = 2; }
-                else { last_ind = 0; memset(num, '\0', sizeof num); state = 0; }
+                else { last_ind = 0; memset(num, '\0', sizeof num); state = 4; }
 
                 break;
 
             case 2:
                 if ( s == 'n' ) { state = 3; }
-                else if ( isdigit(s) ) { last_ind = 1; memset(num, '\0', sizeof num);; num[0] = s; state = 1; }
-                else { last_ind = 0; memset(num, '\0', sizeof num); state = 0; }
+                else { last_ind = 0; memset(num, '\0', sizeof num); state = 4; }
 
                 break;
 
             case 3:
-                answer = strtol(num, NULL, 10) * 25.4;
-                printf("%.2fmm\n", answer);
+                if ( !isdigit(s) && !isalpha(s) ) { answer = strtol(num, NULL, 10) * 25.4; printf("%.2fmm\n", answer); last_ind = 0; memset(num, '\0', sizeof num); state = 0; }
+                else { memset(num, '\0', sizeof num); last_ind = 0; state = 4; }
 
-                if ( isdigit(s) ) { memset(num, '\0', sizeof num);; last_ind = 1; num[0] = s; state = 1; }
-                else { memset(num, '\0', sizeof num); last_ind = 0; state = 0; }
+                break;
+
+            case 4:
+                if ( !isdigit(s) && !isalpha(s) ) { state = 0; }
+                else { state = 4; }
 
                 break;
         }
     }
 
-    if (state == 3){
+    if (state == 3) {
         answer = strtol(num, NULL, 10) * 25.4;
         printf("%.2fmm\n", answer);
     }
